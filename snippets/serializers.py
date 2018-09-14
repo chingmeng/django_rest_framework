e295f7de674dabe5f31c6d86c3ab004be47fe522
Meng <meng@map72.com>
2018-09-14 09:38:46 +0800

    Preparing Authentication and permission (Tutorial 4.1)

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,14 +1,24 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
+
 
 # Method 2 for serializing, a faster way to create serializer class
 
 class SnippetSerializer(serializers.ModelSerializer):
+    # owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
-
-
 
 # Method 1 for serializing
 
